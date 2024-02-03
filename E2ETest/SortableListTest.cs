38e2d103ea4ef5cf20f8e1f58f48a81fe86fe61c
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2024-02-04 07:55:27 +0900

    testid

---
+++ E2ETest/SortableListTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2ETest;
+
+public class SortableListTest : PageTest
+{
+    public override BrowserNewContextOptions ContextOptions()
+    {
+        return new BrowserNewContextOptions()
+        {
+            BaseURL = "https://red-mushroom-0fcb8c710.4.azurestaticapps.net/",
+        };
+    }
+
+    [Test]
+    public async Task SortableList()
+    {
+        await Page.GotoAsync("/");
+
+        await Page.GetByTestId("sortablelist").IsVisibleAsync();
+        await Page.GetByTestId("sortablelist").ClickAsync();
+
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("BlazorApp26"));
+
+        await Page.GetByTestId("sortablelisttitle").IsVisibleAsync();
+
+    }
+}
---
+++ E2ETest/UnitTest1.cs
@@ -0,0 +1,28 @@
+using Microsoft.Playwright;
+
+namespace E2ETest;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
+{
+    public override BrowserNewContextOptions ContextOptions()
+    {
+        return new BrowserNewContextOptions()
+        {
+            BaseURL = "https://red-mushroom-0fcb8c710.4.azurestaticapps.net/",
+        };
+    }
+
+
+
+    [Test]
+    public async Task Home()
+    {
+        await Page.GotoAsync("/");
+
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("BlazorApp26"));
+
+    }
+}
